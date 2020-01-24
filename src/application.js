580fc0d93f98323b2fb586760d0d897578ad5c6e
tadamory <fortross59@gmail.com>
2020-01-25 01:04:43 +0500

    setup project

---
+++ src/application.js
@@ -0,0 +1,14 @@
+const click = () => {
+  //const xhr = new XMLHttpRequest();
+  //xhr.open('GET', 'https://api.twitter.com/1.1/statuses/home_timeline.json, false');
+  //xhr.send();
+  //alert(xhr.responseText);
+  //const p = document.createElement('p');
+  //p.textContext = 'sdfsdf';
+  //document.body.append(p);
+  alert('sdfs');
+};
+
+export default () => {
+	document.querySelector('.btn').addEventListener('click', click);
+};
