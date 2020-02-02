e6375219b6b484578c5521dd0b219e977fe6b23f
tadamory <fortross59@gmail.com>
2020-02-02 10:51:56 +0500

    update type files

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import Button from './components/Button.jsx';
-
-ReactDOM.render(
-  <Button />, 
-  document.getElementById('button'),
-);
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.jsx',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main_bundle.js',
