30b0a7901547d6f0902203b9ddc48649938e7b24
tadamory <fortross59@gmail.com>
2020-02-10 20:21:41 +0500

    bug fix eslint

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { connect } from "react-redux";
 
 import { updateData } from "../actions";
 import getDataFromTinkoff from "../tinkoff";
 import dataSelector from "../selectors";
+
+import _ from 'lodash';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -20,19 +21,19 @@ const App = () => {
     }
     const listItems = content.map(element => {
       const { name, currency } = element;
-      return <li>{`${name} (${currency})`}</li>;
+      return <li key={_.uniqueId()}>{`${name} (${currency})`}</li>;
     });
     return <ul>{listItems}</ul>;
   };
 
   return (
-    <div class="container-fluid mb-4 mt-4">
+    <div className="container-fluid mb-4 mt-4">
       <h1>Tinkoff API</h1>
-      <button type="button" class="btn btn-primary" onClick={handleClick}>
+      <button type="button" className="btn btn-primary" onClick={handleClick}>
         Get data
       </button>
       <h2>Stock list:</h2>
-      <div class="container-fluid mb-4 ml-4">{renderElements(data)}</div>
+      <div className="container-fluid mb-4 ml-4">{renderElements(data)}</div>
     </div>
   );
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,15 @@
 import { combineReducers } from "redux";
 
 const dataFromTinkoff = (state = [], action) => {
-	switch (action.type) {
-		case "DATA_UPDATE": {
-			return action.payload.dataFromTinkoff;
-		}
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case "DATA_UPDATE": {
+      return action.payload.dataFromTinkoff;
+    }
+    default:
+      return state;
+  }
 };
 
 export default combineReducers({
-	dataFromTinkoff
+  dataFromTinkoff
 });
